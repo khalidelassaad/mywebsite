a1520885bfd9addd590a3c34d6a6bdb3e1b30036
Khalid <khalidelassaad@gmail.com>
2022-01-20 16:45:52 -0800

    code: css for headerbar

--- src/header/headerBar.tsx
+++ src/header/headerBar.tsx
@@ -1,6 +1,9 @@
 import * as React from 'react';
 import HeaderButton from './headerButton';
 
+const listItemClassName: string = 'headerBar-listItem';
+const listClassName: string = 'headerBar-list';
+
 interface Props {
   labels: string[];
   onClicks: (() => any)[];
@@ -8,10 +11,10 @@ interface Props {
 
 function HeaderBar({ labels, onClicks }: Props) {
   return (
-    <ul>
+    <ul className={listClassName}>
       {labels.map((label, index) => {
         return (
-          <li key={label}>
+          <li key={label} className={listItemClassName}>
             <HeaderButton key={label} label={label} onClick={onClicks[index]} />
           </li>
         );
