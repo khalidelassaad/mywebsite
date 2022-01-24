2e56c181881c779388848ee50dd98075086077de
Khalid <khalidelassaad@gmail.com>
2022-01-23 17:40:48 -0800

    code: wrap app in react router

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,14 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById('root'),
 );
 
