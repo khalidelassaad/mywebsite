72ac8a4ea50276299fc7e2263994dca86f60cc60
Khalid <khalidelassaad@gmail.com>
2022-01-28 18:13:10 -0800

    code: configured HeaderButton to optionally use hover menu

--- src/header/HeaderButton.tsx
+++ src/header/HeaderButton.tsx
@@ -1,23 +1,42 @@
 import * as React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { HeaderHoverMenu } from './HeaderHoverMenu';
 
 const buttonClassName: string = 'headerBar-button';
 
 interface HeaderButtonProps {
   label: string;
   linkTo: string;
+  hoverMenuButtonPropsList?: HeaderButtonProps[];
 }
 
-function HeaderButton({ label, linkTo }: HeaderButtonProps) {
+function HeaderButton(props: HeaderButtonProps) {
   const navigate = useNavigate();
+  const [isMouseOver, setIsMouseOver] = React.useState(false);
+
+  const onMouseOver = () => {
+    setIsMouseOver(true);
+  };
+  const onMouseOut = () => {
+    setIsMouseOver(false);
+  };
+
   return (
     <div
       className={buttonClassName}
       onClick={() => {
-        navigate(linkTo);
+        navigate(props.linkTo);
       }}
     >
-      {label}
+      {props.label}
+      {props.hoverMenuButtonPropsList == undefined ? (
+        <></>
+      ) : (
+        <HeaderHoverMenu
+          childButtonPropsList={props.hoverMenuButtonPropsList}
+          visible={isMouseOver}
+        />
+      )}
     </div>
   );
 }
